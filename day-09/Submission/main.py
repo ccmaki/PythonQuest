5bdcd7bbc46fea24baed2e34089106327254174a
ccmaki <150521593+ccmaki@users.noreply.github.com>
2024-02-15 02:01:29 +0800

    Update main.py

---
+++ day-09/Submission/main.py
@@ -0,0 +1,10 @@
+with open('day-09\story.txt','r') as file:
+    count = len(file.readlines())
+    print("story.txt has %d lines" % (count))
+    
+with open('day-09\story.txt','r') as file:    
+    content = file.read()
+    print(content)
+    
+
+    
