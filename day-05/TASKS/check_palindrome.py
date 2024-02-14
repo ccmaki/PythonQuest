d22b514d4d672a802e5caba6c4d8200714bbb5e6
ccmaki <150521593+ccmaki@users.noreply.github.com>
2024-02-15 01:05:08 +0800

    Update check_palindrome.py

--- day-05/TASKS/check_palindrome.py
+++ day-05/TASKS/check_palindrome.py
@@ -1,25 +1,17 @@
 # Write the code ↓ to prompt the user to enter a word.
 # Be cautious when reading input of various data types.
-
-
-
-
-
-
+print("PALINDROME CHECKER FOR ALF")
+word = input("Please, enter a word/s to check: ")
 
 # Write the code ↓ to check if the entered word is a palindrome.
 # Utilize string functions to compare the original word with its reverse.
-
-
-
-
+r_word = word[::-1]
 
 
 # Write the code ↓ to display whether the entered word is a palindrome or not.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
-
-
-
-
-
+if r_word == word:
+    print("'%s' is a palindrome." % (word))
+else:
+    print("'%s' is not a palindrome." % (word))
 
