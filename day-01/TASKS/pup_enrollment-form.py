a7e7b61b9e0c870a4a13af27bd20a1775f86c085
ccmaki <150521593+ccmaki@users.noreply.github.com>
2024-02-15 00:56:26 +0800

    Update pup_enrollment-form.py

--- day-01/TASKS/pup_enrollment-form.py
+++ day-01/TASKS/pup_enrollment-form.py
@@ -1,14 +1,24 @@
 # Write the code ↓ to read user's input.
 # Be cautious when reading input of various data types.
-
-
-
+print("PUP Enrollment Form\n")
+name = input("Enter your full name: ")
+age = input("Enter your age: ")
+average = float(input("Enter your previous weighted average: "))
+is_member = input("Are you a member of the AWS Cloud CLub: ").lower() == "yes"
 
 
 
 # Write the code ↓ to display the user's personal information.
 # Select and employ a string concatenation method based on your personal preference and comfort level.
-
+print("\n")
+print("Your Enrollment form")
+print("Name: "+name)
+print("Age: "+age)
+print("GWA:",average)
+if is_member == 1:
+    print("AWStraunot: True")
+else:
+    print("AWStraunot: False")
 
 
 
