db8b275656dcf2de126dd972f4325f4cafc1cb4f
ccmaki <150521593+ccmaki@users.noreply.github.com>
2024-02-20 22:45:51 +0800

    Update main.py

--- day-13/Submission/main.py
+++ day-13/Submission/main.py
@@ -1,15 +1,24 @@
 from flask import Flask, jsonify  
+import csv
 
 app = Flask(__name__)
 
 csv_file = 'programming_languages.csv'
 
-def read_csv():
-    pass
+def read_csv(filename):
+    content = {}
+    i = 0
+    with open(filename, 'r') as file:
+        reader = csv.DictReader(file)
+        for line in reader:
+            content[i] = line
+            i = i+1
+    return content
 
 @app.route('/', methods=['GET'])
 def get_langs():
-    pass
+    data = read_csv(csv_file)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
